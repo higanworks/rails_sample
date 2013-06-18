94c5997dda2e6d8d7df4a0d1d315bf8843054147
sawanoboly <sawanoboriyu@higanworks.com>
2013-06-18 15:42:20 +0900

    scaffold book

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+RailsSample::Application.config.secret_token = 'ed0926f2999cb8862fe11def417a21aec64dee05640ef260e8d65470f534cb6aa93f7f8a0e35cd87bfaeebef15b32126f616510e6b995add85e19c9ec00f6eab'
