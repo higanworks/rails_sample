1411740cf0912e8619feba8cd096e9e485464e46
sawanoboly <sawanoboriyu@higanworks.com>
2014-02-19 11:18:03 +0900

    remove deploy for ey

--- deploy/before_restart.rb
+++
@@ -1,6 +0,0 @@
-execute "task assets precompile #{release_path}" do
-  cwd release_path
-  command "bundle exec rake assets:precompile"
-  environment "RAILS_ENV" => 'production',
-              "RAILS_GROUP" => 'assets'
-end
